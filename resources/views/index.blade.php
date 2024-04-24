f1348d93f0b3f2e9c6e76501f66bf49600473e5f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-24 20:29:10 +0200

    chore: remove unused class

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Nothing to see here</title>
     </head>
-    <body class="antialiased">
+    <body>
         <main>
             <p>Nothing to see here</p>
         </main>
