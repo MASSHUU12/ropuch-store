1509df9a5d3b83d7657a3c48e80036a50b17530d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-28 19:26:20 +0200

    feat: implement logging out

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,5 @@ Route::post('/register', [AuthController::class, 'register']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
