6f1283a78719bc24f1f39427f1935e99b93f9bb1
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-05-20 14:39:17 +0200

    feat: add setup controller for handling setup process

---
+++ app/Http/Controllers/SetupController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Setting;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SetupController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): Response
+    {
+        $setup_complete = Setting::where('key', 'setup_complete')
+            ->firstOrCreate(
+                ['key' => 'setup_complete'],
+                ['value' => 'false']
+            );
+
+        if ($setup_complete['value'] === 'true') return abort(404);
+
+        $response = (new AuthController())->register($request, "admin");
+
+        if ($response->getStatusCode() === 201) {
+            $setup_complete->value = 'true';
+            $setup_complete->save();
+        }
+
+        return $response;
+    }
+}
