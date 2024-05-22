f213c167a49dc74c894963d5f01c55ba44ed1246
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-05-22 13:58:31 +0200

    feat: add EmployeeFactory for generating employee instances

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => fake()->unique()->numberBetween(1, 10),
+            'joining_date' => fake()->date(),
+            'role' => fake()->randomElement(['admin', 'manager', 'employee'])
+        ];
+    }
+}
