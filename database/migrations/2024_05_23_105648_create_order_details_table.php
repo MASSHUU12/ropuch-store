126dac595b171e9b550279767d9f3ef7641cce75
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-05-23 13:11:52 +0200

    feat: create migration for order details table

---
+++ database/migrations/2024_05_23_105648_create_order_details_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->string('buyer_name');
+            $table->string('buyer_email');
+            $table->string('delivery_address');
+            $table->string('delivery_city');
+            $table->string('delivery_state');
+            $table->string('delivery_zip');
+            $table->string('delivery_country');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
