a5e5ba078f3fd88cf03bcc192387610a6d4cf295
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-05-23 13:11:44 +0200

    feat: improve foreign key constraints in migrations

--- database/migrations/2024_05_17_095822_create_payments_table.php
+++ database/migrations/2024_05_17_095822_create_payments_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("order_id");
-            $table->foreign("order_id")->references("id")->on("orders");
+            $table->foreignId("order_id")
+                ->constrained('orders')
+                ->onDelete('cascade');
             $table->string("payment_method", 64);
-            $table->string("transaction_id", 256);
+            $table->string("transaction_id", 256)->unique();
             $table->decimal("amount", 10, 2);
             $table->string("currency", 3);
             $table->string("status", 128);
