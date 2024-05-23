8cf606cd15e8e676528d185fff8bebb7d3e844bf
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-05-23 13:28:50 +0200

    feat: register OrderDetailsSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             OrderSeeder::class,
             OrderItemSeeder::class,
+            OrderDetailsSeeder::class,
             PaymentSeeder::class,
             ShoppingCartSeeder::class,
         ]);
